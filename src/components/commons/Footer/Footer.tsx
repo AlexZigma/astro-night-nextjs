544c126d8a5a9effef4057f15da4665f4a356a92
Alex <tor.trapeczia@gmail.com>
2026-02-25 16:40:45 +0300

    change footer: date

--- src/components/commons/Footer/Footer.tsx
+++ src/components/commons/Footer/Footer.tsx
@@ -1,12 +1,13 @@
 import styles from "./Footer.module.scss";
 
 export default function Footer() {
+  const date = new Date().getFullYear();
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContent}>
         <p className={styles.footerTitle}>.all rights reserved.</p>
         <p className={styles.footerSubtitle}>
-          Copyright © 2025 SALT AND PEPPER
+          Copyright © {date} SALT AND PEPPER
         </p>
       </div>
     </footer>
