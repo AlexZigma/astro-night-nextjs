816627fcf54a56115761e04b4b1d3b6d26e905b7
Alex <tor.trapeczia@gmail.com>
2026-03-04 21:49:49 +0300

    add layout: ModalProvider

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,16 +3,18 @@ import React from "react";
 import Footer from "@/components/commons/Footer";
 import Header from "@/components/commons/Header";
 
+import { ModalProvider } from "./ModalProvider";
+
 export default function MainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <>
+    <ModalProvider>
       <Header />
       {children}
       <Footer />
-    </>
+    </ModalProvider>
   );
 }
