1587b38d97dd1bfdff97e3c4c3a06c0ef8ceec39
Alex <tor.trapeczia@gmail.com>
2026-03-04 21:49:26 +0300

    add cta: modal opening

--- src/components/pages/HomePage/CTASection/CTASection.tsx
+++ src/components/pages/HomePage/CTASection/CTASection.tsx
@@ -1,13 +1,17 @@
+import { useModal } from "@/app/(main)/ModalProvider";
 import SmallButton from "@/components/commons/Button/SmallButton";
 
 import styles from "./cta.module.scss";
 
 export default function CTASection() {
+  const { openModal } = useModal();
   return (
     <section className={styles.cta}>
       <div className={styles.ctaContent}>
         <p className={styles.ctaTitle}>Would you like to add something?</p>
-        <SmallButton active>.yes.</SmallButton>
+        <SmallButton variant="active" onClick={() => openModal()}>
+          .yes.
+        </SmallButton>
       </div>
     </section>
   );
