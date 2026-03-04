624833b5750ce90887cf5b9c05d467854f8d449f
Alex <tor.trapeczia@gmail.com>
2026-03-04 20:49:55 +0300

    add tags: const and type

---
+++ src/models/tags/consts.ts
@@ -0,0 +1,13 @@
+import { Tag } from "./types";
+
+export const TAGS: Tag[] = [
+  { label: "noir", isChecked: false },
+  { label: "horror", isChecked: false },
+  { label: "action", isChecked: false },
+  { label: "comedy", isChecked: false },
+  { label: "animation", isChecked: false },
+  { label: "romantic", isChecked: false },
+  { label: "ski-fi", isChecked: false },
+  { label: "drama", isChecked: false },
+  { label: "advanture", isChecked: false },
+];
