4ac3299e084264e7388b6928089bb6035301b009
Alex <tor.trapeczia@gmail.com>
2026-03-04 21:50:35 +0300

    add HomePage: modal component

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,15 +1,23 @@
+"use client";
+
+import MovieModal from "@/components/commons/Modal/MovieModal";
 import CTASection from "@/components/pages/HomePage/CTASection";
 import HeroSection from "@/components/pages/HomePage/HeroSection";
 import InfoSection from "@/components/pages/HomePage/InfoSection";
 import TopMoviesSection from "@/components/pages/HomePage/TopMoviesSection";
 
+import { useModal } from "./ModalProvider";
+
 export default function Home() {
+  const { isModalOpen } = useModal();
+
   return (
     <main>
       <HeroSection />
       <InfoSection />
       <TopMoviesSection />
       <CTASection />
+      {isModalOpen && <MovieModal />}
     </main>
   );
 }
