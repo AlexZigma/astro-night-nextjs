62f95500feb5812d3825abd9d1bb4ee46c1fd4de
Alex <tor.trapeczia@gmail.com>
2026-03-06 12:39:20 +0300

    remove layout: provider

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,18 +3,16 @@ import React from "react";
 import Footer from "@/components/commons/Footer";
 import Header from "@/components/commons/Header";
 
-import { ModalProvider } from "./ModalProvider";
-
 export default function MainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ModalProvider>
+    <>
       <Header />
       {children}
       <Footer />
-    </ModalProvider>
+    </>
   );
 }
