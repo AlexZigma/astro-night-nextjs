aced61b00374ab9d11f60154f8b8f870a3695a5d
Alex <tor.trapeczia@gmail.com>
2026-03-06 12:37:33 +0300

    change card: props and composition

--- src/components/commons/Card/Card.tsx
+++ src/components/commons/Card/Card.tsx
@@ -1,40 +1,57 @@
 import clsx from "clsx";
 import Image from "next/image";
+import Link from "next/link";
 
 import styles from "./card.module.scss";
 
 interface CardProps {
-  imgSrc: string;
-  imgAlt: string;
+  id: string;
+  image: string;
   title: string;
-  subtitle: string;
+  year: string;
   rating: string;
   small?: boolean;
 }
 
+export function CardPoster({
+  rating,
+  image,
+  className,
+}: {
+  rating: string;
+  image: string;
+  className?: string;
+}) {
+  return (
+    <div className={clsx(styles.cardPoster, className)}>
+      <Image
+        className={styles.cardImg}
+        src={image}
+        alt="movie card"
+        // width={280}
+        // height={400}
+        fill
+      />
+      <span className={styles.cardRating}>{rating}</span>
+    </div>
+  );
+}
+
 export default function Card({
-  imgSrc,
-  imgAlt,
+  id,
+  image,
   title,
-  subtitle,
+  year,
   rating,
   small,
 }: CardProps) {
   return (
     <div className={clsx(styles.card, small && styles.cardSmall)}>
-      <div className={styles.cardTop}>
-        <Image
-          className={styles.cardImg}
-          src={imgSrc}
-          alt={imgAlt}
-          width={280}
-          height={400}
-        />
-        <span className={styles.cardRating}>{rating}</span>
-      </div>
+      <Link className={styles.cardLink} href={`/items/${id}`} />
+      <CardPoster rating={rating} image={image} />
       <div className={styles.cardInfo}>
         <p className={styles.cardTitle}>{title}</p>
-        <p className={styles.cardSubtitle}>{subtitle}</p>
+        <p className={styles.cardSubtitle}>{year}</p>
       </div>
     </div>
   );
