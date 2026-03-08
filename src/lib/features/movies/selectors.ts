f19f8c20194307a233de6ff20ca4763d301ebd4f
Alex <tor.trapeczia@gmail.com>
2026-03-08 15:45:40 +0300

    add selectors: status selector

--- src/lib/features/movies/selectors.ts
+++ src/lib/features/movies/selectors.ts
@@ -2,5 +2,7 @@ import { RootState } from "@/lib/store";
 
 export const selectMovies = (state: RootState) => state.movies.items;
 
+export const selectStatus = (state: RootState) => state.movies.status;
+
 export const selectMovieById = (id: string) => (state: RootState) =>
   state.movies.items.find((movie) => movie.id === id);
