9d34c01f94a896b2e0d559c9b52e6ba7e43fd2f4
Alex <tor.trapeczia@gmail.com>
2026-03-08 18:18:37 +0300

    add selectors: selectors

--- src/lib/features/movies/selectors.ts
+++ src/lib/features/movies/selectors.ts
@@ -1,6 +1,18 @@
+import { createSelector } from "@reduxjs/toolkit";
+
 import { RootState } from "@/lib/store";
 
-export const selectMovies = (state: RootState) => state.movies.items;
+export const selectMovies = (state: RootState) => {
+  return state.movies.items;
+};
+
+export const selectFirst8Movies = createSelector([selectMovies], (movies) =>
+  movies.slice(0, 8),
+);
+
+export const selectTop10Movies = createSelector([selectMovies], (movies) =>
+  [...movies].sort((a, b) => Number(b.rating) - Number(a.rating)).slice(0, 10),
+);
 
 export const selectStatus = (state: RootState) => state.movies.status;
 
