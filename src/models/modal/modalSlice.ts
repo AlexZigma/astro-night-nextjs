d25872a828957c21ff4f7ba4196ccf213b573804
Alex <tor.trapeczia@gmail.com>
2026-03-11 17:41:47 +0300

    add modal: current movie state

--- src/models/modal/modalSlice.ts
+++ src/models/modal/modalSlice.ts
@@ -1,18 +1,24 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
+import { Movie } from "../movies/types";
 import { ModalState } from "./types";
 
-const initialState: ModalState = { isOpen: false };
+const initialState: ModalState = {
+  isOpen: false,
+  currentMovie: null,
+};
 
 export const modalSlice = createSlice({
   name: "modal",
   initialState,
   reducers: {
-    openModal: (state) => {
+    openModal: (state, action: PayloadAction<Movie | undefined>) => {
       state.isOpen = true;
+      state.currentMovie = action.payload;
     },
     closeModal: (state) => {
       state.isOpen = false;
+      state.currentMovie = null;
     },
   },
 });
--- src/models/modal/types.ts
+++ src/models/modal/types.ts
@@ -1,3 +1,11 @@
+import { Movie } from "../movies/types";
+
 export type ModalState = {
   isOpen: boolean;
+  currentMovie?: Movie | null;
 };
+
+export enum ModalVariant {
+  Add = "add",
+  Edit = "edit",
+}
