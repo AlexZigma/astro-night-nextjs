27219c83fb323376a7cb4a0319756e354243b21b
Alex <tor.trapeczia@gmail.com>
2026-03-11 17:41:03 +0300

    add movieReducer: delete and edit actions

--- src/models/movies/moviesSlice.ts
+++ src/models/movies/moviesSlice.ts
@@ -26,6 +26,14 @@ export const moviesSlice = createSlice({
         };
       },
     },
+    deleteMovie: (state, action: PayloadAction<string>) => {
+      state.items = state.items.filter((item) => item.id !== action.payload);
+    },
+    editMovie: (state, action: PayloadAction<Movie>) => {
+      state.items = state.items.map((item) =>
+        item.id === action.payload.id ? action.payload : item,
+      );
+    },
     initializeMovies: (state, action: PayloadAction<Movie[]>) => {
       state.items = action.payload;
       state.status = LoadingStatus.Succeeded;
@@ -33,5 +41,6 @@ export const moviesSlice = createSlice({
   },
 });
 
-export const { addMovie, initializeMovies } = moviesSlice.actions;
+export const { addMovie, deleteMovie, editMovie, initializeMovies } =
+  moviesSlice.actions;
 export default moviesSlice.reducer;
