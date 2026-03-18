fac321a01a1e6798d7fe40480de8e056b55b7796
Alex <tor.trapeczia@gmail.com>
2026-03-18 15:09:21 +0300

    add tags: genres

--- src/models/tags/constants.ts
+++ src/models/tags/constants.ts
@@ -1,13 +1,21 @@
 import { Genre } from "./types";
 
 export const GENRES: Genre[] = [
+  "drama",
+  "animation",
+  "sci-fi",
+  "comedy",
+  "action",
   "noir",
   "horror",
-  "action",
-  "comedy",
-  "animation",
   "romantic",
-  "ski-fi",
-  "drama",
+  "fantasy",
+  "crime",
+  "western",
+  "musical",
+  "historical",
+  "thriller",
+  "documentary",
+  "mystery",
   "advanture",
 ] as const;
