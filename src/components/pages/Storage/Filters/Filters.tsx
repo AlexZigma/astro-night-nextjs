4b96ecbccfb1dec539fe8fbdba7e96c1c4c25cb0
Alex <tor.trapeczia@gmail.com>
2026-03-18 16:00:51 +0300

    add FIlters: use store state

--- src/components/pages/Storage/Filters/Filters.tsx
+++ src/components/pages/Storage/Filters/Filters.tsx
@@ -1,15 +1,37 @@
+import { useCallback } from "react";
+
 import SmallButton from "@/components/commons/Button/SmallButton";
+import { useAppDispatch, useAppSelector } from "@/lib/hooks";
+import { toggleFilterGenre } from "@/models/movies/moviesSlice";
+import { selectFilterGenres } from "@/models/movies/selectors";
 import { GENRES } from "@/models/tags/constants";
+import { Genre } from "@/models/tags/types";
 
 import styles from "./filters.module.scss";
 
 export default function Filters() {
+  const dispatch = useAppDispatch();
+  const filterGenres = useAppSelector(selectFilterGenres);
+
+  const handleFilterChange = useCallback(
+    (genre: Genre) => () => {
+      dispatch(toggleFilterGenre(genre));
+    },
+    [dispatch],
+  );
+
   return (
     <section className={styles.filters}>
-      <p className={styles.filtersTitle}>Filter ( 0 )</p>
+      <p className={styles.filtersTitle}>Filter ( {filterGenres.length} )</p>
       <div className={styles.filtersList}>
-        {GENRES.map((tag) => (
-          <SmallButton key={tag}>{tag}</SmallButton>
+        {GENRES.map((genre) => (
+          <SmallButton
+            key={genre}
+            onClick={handleFilterChange(genre)}
+            variant={filterGenres.includes(genre) ? "active" : undefined}
+          >
+            {genre}
+          </SmallButton>
         ))}
       </div>
     </section>
