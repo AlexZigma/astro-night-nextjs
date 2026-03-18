77a2ae97f39e2ad117b1a214da9bd57f5f27a1d9
Alex <tor.trapeczia@gmail.com>
2026-03-18 15:58:38 +0300

    add movie: filter and sort selectors

--- src/models/movies/selectors.ts
+++ src/models/movies/selectors.ts
@@ -2,9 +2,14 @@ import { createSelector } from "@reduxjs/toolkit";
 
 import { RootState } from "@/lib/store";
 
-export const selectMovies = (state: RootState) => {
-  return state.movies.items;
-};
+export const selectMovies = (state: RootState) => state.movies.items;
+
+export const selectStatus = (state: RootState) => state.movies.status;
+
+export const selectSortField = (state: RootState) => state.movies.sort.field;
+export const selectSortOrder = (state: RootState) => state.movies.sort.order;
+export const selectFilterGenres = (state: RootState) =>
+  state.movies.filters.genres;
 
 export const selectFirst8Movies = createSelector([selectMovies], (movies) =>
   movies.slice(0, 8),
@@ -14,7 +19,38 @@ export const selectTop10Movies = createSelector([selectMovies], (movies) =>
   [...movies].sort((a, b) => b.rating - a.rating).slice(0, 10),
 );
 
-export const selectStatus = (state: RootState) => state.movies.status;
+export const selectFilteredMovies = createSelector(
+  [selectMovies, selectFilterGenres],
+  (movies, filterGenres) =>
+    movies.filter((movie) =>
+      filterGenres.every((genre) => movie.genres.includes(genre)),
+    ),
+);
+
+export const selectFilteredSortedMovies = createSelector(
+  [selectFilteredMovies, selectSortOrder, selectSortField],
+  (movies, sortOrder, sortField) => {
+    if (!sortField || !sortOrder) return movies;
+
+    const sortSign = sortOrder === "asc" ? 1 : -1;
+
+    return movies.toSorted((a, b) => {
+      switch (sortField) {
+        case "rating":
+          return (a.rating - b.rating) * sortSign;
+        case "title":
+          return a.title.localeCompare(b.title) * sortSign;
+        case "year":
+          return (Number(a.year) - Number(b.year)) * sortSign;
+        default:
+          return 0;
+      }
+    });
+  },
+);
+
+export const selectIsFilterUsed = (state: RootState) =>
+  state.movies.filters.genres.length > 0;
 
 export const selectMovieById = (id: string) => (state: RootState) =>
   state.movies.items.find((movie) => movie.id === id);
