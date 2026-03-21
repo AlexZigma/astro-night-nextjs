d78cb3874f7308cbc5d3e328cff9ced7930fd779
Alex <tor.trapeczia@gmail.com>
2026-03-21 18:01:46 +0300

    change nav: remove extra link

--- src/models/nav/constants.ts
+++ src/models/nav/constants.ts
@@ -3,5 +3,4 @@ import { NavItem } from "./types";
 export const NAV_ITEMS: NavItem[] = [
   { link: "/storage", label: ".storage." },
   { link: "/about", label: ".about." },
-  { link: "/search", label: ".search." },
 ];
