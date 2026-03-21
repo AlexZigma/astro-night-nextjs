e43620f918e9e22c73ddcaf651f047a13712638b
Alex <tor.trapeczia@gmail.com>
2026-03-21 18:17:33 +0300

    refactor carousel: card props

--- src/components/pages/HomePage/TopMoviesSection/Carousel.tsx
+++ src/components/pages/HomePage/TopMoviesSection/Carousel.tsx
@@ -39,7 +39,7 @@ export default function Carousel({ movies }: { movies: Movie[] }) {
       <ul ref={spinnerRef} className={styles.carouselList}>
         {movies.map((item) => (
           <li className={styles.carouselItem} key={item.id} id={`#${item.id}`}>
-            <Card {...item} isSmall={true} />
+            <Card {...item} variant="small" />
           </li>
         ))}
       </ul>
