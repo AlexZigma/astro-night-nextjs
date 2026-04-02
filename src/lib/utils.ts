e40365b0335332b5ba843959b65a5f0e0df93b2c
Alex <tor.trapeczia@gmail.com>
2026-04-02 14:56:30 +0300

    add utils: img to base64 function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,18 @@
 export function numberToRating(rating: number) {
   return rating.toFixed(1).toString().replace(".", ",");
 }
+
+export const imgToBase64 = (file: File): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      const base64 = reader.result as string;
+      resolve(base64);
+    };
+    reader.onerror = (error) => {
+      reject(error);
+    };
+
+    reader.readAsDataURL(file);
+  });
+};
