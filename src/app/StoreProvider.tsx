ee3a5e6b0e17eb381809bb1c070250bc0461ff55
Alex <tor.trapeczia@gmail.com>
2026-05-05 00:28:40 +0300

    refactor StoreProvider: remove unused dispatch

--- src/app/StoreProvider.tsx
+++ src/app/StoreProvider.tsx
@@ -1,9 +1,8 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Provider } from "react-redux";
 
 import { AppStore, makeStore } from "@/lib/store";
-import { initializeMoviesRequest } from "@/models/movies/moviesSlice";
 
 export default function StoreProvider({
   children,
@@ -11,10 +10,6 @@ export default function StoreProvider({
   children: React.ReactNode;
 }) {
   const [store] = useState<AppStore>(() => makeStore());
-
-  useEffect(() => {
-    store.dispatch(initializeMoviesRequest());
-  }, [store]);
 
   return <Provider store={store}>{children}</Provider>;
 }
